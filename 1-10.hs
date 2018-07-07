e9f305b44892c0b771987851819a068c341c816b
TahaMagdy <tahamagdy314@gmail.com>
2018-07-07 08:06:51 +0200

    pack

--- 1-10.hs
+++ 1-10.hs
@@ -1,3 +1,6 @@
+{-- Take a look in Data.List;
+  -- It has got tons of useful functions you can learn from.
+--}
 -- | Problem 1
 -- | (*) Find the last element of a list.
 -- |
@@ -234,7 +237,22 @@ compress (x:xs)
 ------
 ------
 ------
-
+-- | Problem 9
+-- | (**) Pack consecutive duplicates of list elements into sublists.
+-- | If a list contains repeated elements they should be placed in separate sublists.
+-- |
+-- | Example:
+-- |
+-- | * (pack '(a a a a b c c a a d e e e e))
+-- | ((A A A A) (B) (C C) (A A) (D) (E E E E))
+-- | Example in Haskell:
+-- |
+-- | *Main> pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a',
+-- |             'a', 'd', 'e', 'e', 'e', 'e']
+-- | ["aaaa","b","cc","aa","d","eeee"]
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = (x:takeWhile (==x) xs) : pack (dropWhile (==x) xs)
 
 
 
