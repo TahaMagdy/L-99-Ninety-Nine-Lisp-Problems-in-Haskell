feafa04449dc255897e6f3449591dda2c73bc59a
TahaMagdy <tahamagdy314@gmail.com>
2018-07-07 07:18:29 +0200

    compress

--- 1-10.hs
+++ 1-10.hs
@@ -206,6 +206,35 @@ flatten (List []) = []
 flatten (Elem x) = [x]
 flatten (List (x:xs)) = flatten x ++ flatten (List (xs))
 
+------
+------
+------
+
+-- |  Problem 8
+-- |  (**) Eliminate consecutive duplicates of list elements.
+-- |
+-- |  If a list contains repeated elements they should be replaced with
+-- |  a single copy of the element. The order of the elements should not be changed.
+-- |
+-- |  Example:
+-- |
+-- |  * (compress '(a a a a b c c a a d e e e e))
+-- |  (A B C A D E)
+-- |  Example in Haskell:
+-- |
+-- |  > compress "aaaabccaadeeee"
+-- |  "abcade"
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:xs)
+  | x == head xs      = compress xs  -- * Ignoring the duplicated x
+  | otherwise         = [x] ++ compress xs -- * Building up the un-duplicated x
+
+------
+------
+------
+
 
 
 
